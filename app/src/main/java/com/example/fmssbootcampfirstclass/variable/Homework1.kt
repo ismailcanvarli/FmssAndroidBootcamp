aaa3af1cb6c3f1cc99ec06529c161b23e1aba2c3
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-12-04 19:47:11 +0300

    Add Kotlin variables and print statements for additional fields from the assignment.

---
+++ app/src/main/java/com/example/fmssbootcampfirstclass/variable/Homework1.kt
@@ -0,0 +1,189 @@
+//Created by canVarli on 12/3/2024
+
+package com.example.fmssbootcampfirstclass.variable
+
+fun main() {
+    // İlçe
+    val district = "Kadıköy"
+    println("İlçe: $district")
+
+    // Kıta
+    val continent = "Avrupa"
+    println("Kıta: $continent")
+
+    // Fax
+    val fax = "0216 123 45 67"
+    println("Fax: $fax")
+
+    // İnstagram adresi
+    val instagramAddress = "instagram.com/kadikoybelediyesi"
+    println("Instagram Adresi: $instagramAddress")
+
+    // Departman
+    val department = "Bilgi İşlem Müdürlüğü"
+    println("Departman: $department")
+
+    // Ürün miktarı
+    val productAmount = 100
+    println("Ürün miktarı: $productAmount")
+
+    // Müşteri soy adı
+    val customerSurname = "Yılmaz"
+    println("Müşteri soy adı: $customerSurname")
+
+    // Ödeme miktarı
+    val paymentAmount = 100.0
+    println("Ödeme miktarı: $paymentAmount")
+
+    // Doğum günü
+    val birthday = "01.01.1990"
+    println("Doğum günü: $birthday")
+
+    // Borç
+    val debt = 100.0
+    println("Borç: $debt")
+
+    // Medeni durum
+    val maritalStatus = "Bekar"
+    println("Medeni Durum: $maritalStatus")
+
+    // Video yorumu
+    val videoComment = "Çok güzel bir video olmuş"
+    println("Video Yorumu: $videoComment")
+
+    // Ödeme tarihi
+    val paymentSaati = "14:00"
+    println("Ödeme Saati: $paymentSaati")
+
+    // EFT miktarı
+    val eftAmount = 100.0
+    println("EFT Miktarı: $eftAmount")
+
+    // Satılan miktar
+    val soldAmount = 100
+    println("Satılan miktar: $soldAmount")
+
+    // Telefon modeli
+    val phoneModel = "Samsung Galaxy S23 ultra"
+    println("Telefon Modeli: $phoneModel")
+
+    // Dergi adı
+    val magazineName = "Bilim ve Teknik"
+    println("Dergi Adı: $magazineName")
+
+    // Basım tarihi
+    val publicationDate = "01.01.2024"
+    println("Basım Tarihi: $publicationDate")
+
+    // Zam miktarı
+    val raiseAmount = 100.0
+    println("Zam Miktarı: $raiseAmount")
+
+    // Daire sayısı
+    val numberOfFlats = 100
+    println("Daire Sayısı: $numberOfFlats")
+
+    // Enlem
+    val latitude = 40.9903
+    println("Enlem: $latitude")
+
+    // Boylam
+    val longitude = 29.1244
+    println("Boylam: $longitude")
+
+    // Yemek adı
+    val foodName = "Mantı"
+    println("Yemek Adı: $foodName")
+
+    // Ürün fiyatı
+    val productPrice = 50.0
+    println("Ürün Fiyatı: $productPrice")
+
+    // Şirket
+    val company = "ABC Teknoloji A.Ş."
+    println("Şirket: $company")
+
+    // Video adı
+    val videoName = "Kadıköy Tanıtım Filmi"
+    println("Video Adı: $videoName")
+
+    // Müzik süresi
+    val musicDuration = "3:45"
+    println("Müzik Süresi: $musicDuration")
+
+    // Mekan puanı
+    val locationRating = 4.5
+    println("Mekan Puanı: $locationRating")
+
+    // Dosya adı
+    val fileName = "sunum.pdf"
+    println("Dosya Adı: $fileName")
+
+    // Resim formatı
+    val imageFormat = "JPEG"
+    println("Resim Formatı: $imageFormat")
+
+    // Renk
+    val color = "Mavi"
+    println("Renk: $color")
+
+    // Renk kodu
+    val colorCode = "#0000FF"
+    println("Renk Kodu: $colorCode")
+
+    // Bilgisayar modeli
+    val computerModel = "Dell XPS 15"
+    println("Bilgisayar Modeli: $computerModel")
+
+    // Ekran boyutu
+    val screenSize = "15.6 inç"
+    println("Ekran Boyutu: $screenSize")
+
+    // Kullanım süresi
+    val usageDuration = "2 yıl"
+    println("Kullanım Süresi: $usageDuration")
+
+    // Basınç
+    val pressure = 1013.25
+    println("Basınç: $pressure")
+
+    // Etkinlik günü
+    val eventDay = "Cumartesi"
+    println("Etkinlik Günü: $eventDay")
+
+    // Ödeme günü
+    val paymentDay = "10.12.2024"
+    println("Ödeme Günü: $paymentDay")
+
+    // Yolculuk çıkış tarihi
+    val departureDate = "15.12.2024"
+    println("Yolculuk Çıkış Tarihi: $departureDate")
+
+    // Mahalle adı
+    val neighborhood = "Moda"
+    println("Mahalle Adı: $neighborhood")
+
+    // Otobüs hattı
+    val busLine = "16A"
+    println("Otobüs Hattı: $busLine")
+
+    // Kullanılan dakika
+    val usedMinutes = 120
+    println("Kullanılan Dakika: $usedMinutes")
+
+    // Kargo takip numarası
+    val trackingNumber = "TR123456789"
+    println("Kargo Takip No: $trackingNumber")
+
+    // Kupon süresi
+    val couponDuration = "30 gün"
+    println("Kupon Süresi: $couponDuration")
+
+    // Kupon kodu
+    val couponCode = "INDIRIM2024"
+    println("Kupon Kodu: $couponCode")
+
+    // Fatura tarihi
+    val invoiceDate = "01.12.2024"
+    println("Fatura Tarihi: $invoiceDate")
+}

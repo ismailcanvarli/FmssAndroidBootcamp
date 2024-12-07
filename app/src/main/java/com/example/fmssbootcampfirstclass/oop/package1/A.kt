9af908414ef6d1cd8eacfd8864a25136862b8f5f
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-12-07 22:09:53 +0300

    Introduce classes to demonstrate basic class concepts.

---
+++ app/src/main/java/com/example/fmssbootcampfirstclass/oop/package1/A.kt
@@ -0,0 +1,11 @@
+//Created by canVarli on 12/7/2024
+
+package com.example.fmssbootcampfirstclass.oop.package1
+
+class A {
+    var temp = 1
+    public var temp1 = 2
+    protected var temp2 = 3
+    private var temp3 = 4
+
+}
---
+++ app/src/main/java/com/example/fmssbootcampfirstclass/oop/package2/B.kt
@@ -0,0 +1,12 @@
+//Created by canVarli on 12/7/2024
+
+package com.example.fmssbootcampfirstclass.oop.package2
+
+import com.example.fmssbootcampfirstclass.oop.package1.A
+
+class B {
+    fun metodB() {
+        val a = A()
+        println("B class calling A class")
+    }
+}

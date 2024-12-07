70a4e3ae1e367323b1e7c5773a2f162c49d46f66
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-12-07 11:44:32 +0300

    Implement static class functionality using companion object in Kotlin

---
+++ app/src/main/java/com/example/fmssbootcampfirstclass/oop/MyClass.kt
@@ -0,0 +1,13 @@
+//Created by canVarli on 12/7/2024
+
+package com.example.fmssbootcampfirstclass.oop
+
+class MyClass {
+    companion object {
+        var variable = 10
+        fun myFunction() {
+            println("This is my function")
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/fmssbootcampfirstclass/oop/StaticClass.kt
@@ -0,0 +1,8 @@
+//Created by canVarli on 12/7/2024
+
+package com.example.fmssbootcampfirstclass.oop
+
+fun main() {
+    println(MyClass.variable)
+    MyClass.myFunction()
+}
